a275af10e78a7921d901edc16b3e6f3154865f86
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 16:17:30 -0500

    implemented setInfo()

--- src/demo1/DemoTest7.java
+++ src/demo1/DemoTest7.java
@@ -3,6 +3,15 @@ package demo1;
 public class DemoTest7 {
 	 public static void main (String args []) {
 		 System.out.println("JavaIsFun");
+		 System.out.println("Layan");
+		 
+		 String name = "Layan";
+		 
+		 System.out.println(name + "Layan");
 	 }
-
+ public void setInfo(String name) {
+	
+	 
+	 System.out.println(name);
+ }
 }
